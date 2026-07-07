2bbe1f6b510f1d921936893ad4b34e2b29035667
sonali19705 <sonalikale19705@gmail.com>
2026-07-07 18:07:03 +0530

    Create initial FoodHub layout with Navbar and Home components

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <nav>
+      <h2>🍕 FoodHub</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to FoodHub</h1>
+      <p>Order your favorite food online.</p>
+    </div>
+  );
+}
+
+export default Home;
